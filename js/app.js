6c67a9c0a047cd21e1b9ff60dae83059fa3c7326
KostyaPenyavskiy <kornelius9898@gmail.com>
2022-09-01 13:46:32 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.burger-menu');
+const form = $('.subscribe__form');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+});
+
+$(document).ready(function(){
+  $('.header__gallery').bxSlider({
+    controls: false,
+    pager: false,
+    speed: 1000,
+    pause: 2000,
+    moveSlides: 1,
+    auto: true,
+    infiniteLoop: true,
+    slideWidth: Number(document.querySelector('.header__gallery').offsetWidth),
+  });
+});
+
+form.submit(e => {
+  e.preventDefault();
+
+  form.fadeOut();
+  $('.subscribe__heading').fadeOut();
+  $('.thankyou').fadeIn();
+});
