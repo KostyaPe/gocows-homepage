e9c60c151239e3ff64e78632c7db48a3830c1257
KostyaPenyavskiy <kornelius9898@gmail.com>
2022-09-01 16:38:41 +0300

    done

---
+++ pages/single-post-sb/js/app.js
@@ -0,0 +1,22 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.burger-menu');
+const form = $('.subscribe__form');
+
+menuBtn.addEventListener('click', function(){
+  console.log('clicked!')
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+});
+
+$(document).ready(function(){
+  $('.header__gallery').bxSlider({
+    controls: false,
+    pager: false,
+    speed: 1000,
+    pause: 2000,
+    moveSlides: 1,
+    auto: true,
+    infiniteLoop: true,
+    slideWidth: Number(document.querySelector('.header__gallery').offsetWidth),
+  });
+});
